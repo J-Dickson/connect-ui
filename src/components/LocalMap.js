d04a6eed3f39786676dd1177a9afd2c70c36a408
Jonathon Dickson <jdickson912@gmail.com>
2018-01-20 11:32:21 +0100

    Heat map now added along with places plotted on the map and marker modals for users and places. Zoom does now not effect position of marker

--- src/components/LocalMap.js
+++ src/components/LocalMap.js
@@ -1,34 +1,29 @@
 import React from 'react'
-import mapboxgl from 'mapbox-gl'
+import MapHelper from '../helpers/MapHelper'
 import './LocalMap.css'
-import geojson from '../data/BigHaq'
 
 class LocalMap extends React.Component {
+  constructor (props) {
+    super(props)
+    this.mapContainer = null
+    this.map = null
+  }
   componentDidMount () {
-    mapboxgl.accessToken = 'pk.eyJ1IjoiamQ5MTIiLCJhIjoiY2pjbWYzbzdxMDN4YTJ5bzBrc2VvdDl6ciJ9.0Cutw6rZNaP2pY58wj1V1w'
-    const map = new mapboxgl.Map({
-      container: 'local-map',
-      style: 'mapbox://styles/mapbox/streets-v10',
-      center: [11.576124, 48.137154],
-      zoom: 8
+    this.map = MapHelper.instantiateMap(this.mapContainer)
+    MapHelper.getUserMarkers(this.map)
+    MapHelper.getPlaceMarkers(this.map)
+    this.map.on('style.load', () => {
+      MapHelper.createHeatMapSource(this.map)
+      MapHelper.getHeatMapLayer(this.map)
     })
-    geojson.features.forEach(marker => {
-      // create a HTML element for each feature
-      let el = document.createElement('div')
-      el.className = 'fa fa-user marker'
-
-      // make a marker for each feature and add to the map
-      new mapboxgl.Marker(el)
-      .setLngLat(marker.geometry.coordinates)
-      .addTo(map)
-    })
+  }
+  componentWillUnmount () {
+    this.map.remove()
   }
   render () {
     return (
       <div className='container'>
-        <div id='local-map'>
-          poop
-        </div>
+        <div ref={el => this.mapContainer = el} />
       </div>
     )
   }
