5a1a11ca0db06764a281a650743d1ddc8b656d75
Chris Crispin <Chris.Crispin@homeretailgroup.com>
2018-01-20 14:52:51 +0100

    header + burger

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,32 @@
 import React from 'react'
 import './header.css'
+import logo from '../assets/fcb-dhl-logo.png'
 
 class Header extends React.Component {
   render () {
     return (
       <div className='header'>
-        HEADER BABY
+        <nav role='navigation'>
+          <div id='menuToggle'>
+            <input type='checkbox' />
+            <i id='close-menu' class="fa fa-times fa-3x" aria-hidden="true"></i>
+            <span />
+            <span />
+            <span />
+            <ul id='menu'>
+              <a href="#"><i class="fa fa-home fa-fw" aria-hidden="true"></i><li>&nbsp; Connect</li></a>
+              <a href="#"><i class="fa fa-comment-o fa-fw" aria-hidden="true"></i><li>&nbsp; Messages</li></a>
+              <a href="#"><i class="fa fa-user-circle-o fa-fw" aria-hidden="true"></i><li>&nbsp; Profile</li></a>
+              <br></br>
+              <a href="#"><i class="fa fa-camera fa-fw" aria-hidden="true"></i><li>&nbsp; Submit a selfie</li></a>
+              <a href="#"><i class="fa fa-trophy fa-fw" aria-hidden="true"></i><li>&nbsp; Competitions</li></a>
+              <a href="#"><i class="fa fa-futbol-o fa-fw" aria-hidden="true"></i><li>&nbsp; FC Bayern</li></a>
+            </ul>
+          </div>
+        </nav>
+        <div id='logo'>
+          <img src={logo} />
+        </div>
       </div>
     )
   }
