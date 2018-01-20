334ab57bec77cd54f39c5874363c10c6ba60d24b
Rachel Newman <rachel.newman@argos.co.uk>
2018-01-20 09:46:26 +0100

    Account and header component added with no styling

--- src/config/routes.js
+++ src/config/routes.js
@@ -4,6 +4,7 @@ import createBrowserHistory from 'history/createBrowserHistory'
 import App from '../components/App'
 import Registration from '../components/Registration'
 import LocalMap from '../components/LocalMap'
+import Account from '../components/Account.jsx'
 const customHistory = createBrowserHistory()
 
 const routes = () =>
@@ -12,6 +13,7 @@ const routes = () =>
       <Route exact path='/' component={App} />
       <Route path='/register' component={Registration} />
       <Route path='/local-area' component={LocalMap} />
+      <Route path='/account' component={Account} />
     </div>
   </Router>
 
